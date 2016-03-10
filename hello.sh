bc51b254748d94e0bc5bcec94e4902b01228e356
章晓健 <809270105@qq.com>
2016-03-10 21:38:24 +0800

    first commit shell

---
+++ hello.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo 'hello world'
+
+himi="test"
+
+echo "A is:"
+echo $himi A
+echo "A $himi_OK"
+echo "A ${himi}_OK"
+
+
+
+#two
+echo ============two========
+
+
+_int=19
+_tot=`expr $_int + 89`
+echo $_tot
---
+++ myshell.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+COUNTER=0
+while [ $COUNTER -lt  5 ]
+do
+    COUNTER=' expr $COUNTER + 1'
+    echo $COUNTER
+done
+
